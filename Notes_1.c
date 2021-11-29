1e61e2103e947a81203a8a2bfd30811e78c6feef
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-11-29 20:01:18 +0530

    Add files via upload

---
+++ Notes_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+    const int a = 10;
+    const int b = 20;
+    printf("The value of constant a is= %d", a);
+    printf("The value of constant b is= %d", b);
+}
