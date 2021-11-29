7b734cf2a5cb7236c26610be7b4d494e461797ff
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-11-29 20:02:48 +0530

    Add files via upload

---
+++ Notes_6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int num;
+    printf("Enter a number ");
+    scanf("%d", &num);
+    if (num < 10)
+    {
+        printf("Number is less than 10");
+    }
+}
